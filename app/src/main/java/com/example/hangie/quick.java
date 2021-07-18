e218ff7607623602a9388aacec972b8ed981aec4
alaka2019 <60387049+alaka2019@users.noreply.github.com>
2021-07-18 22:29:44 +0530

    over

--- app/src/main/java/com/example/hangie/quick.java
+++ app/src/main/java/com/example/hangie/quick.java
@@ -13,196 +13,208 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class quick extends AppCompatActivity {
-    TextView text, failed, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
-    ImageView h1,h2,h3,h4,h5,h6,h7;
-    ArrayList<String> words=new ArrayList<String>();
-    ArrayList<Integer> order=new ArrayList<Integer>();
+    TextView text, failed, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    ImageView h1, h2, h3, h4, h5, h6, h7;
+    ArrayList<String> words = new ArrayList<String>();
+    ArrayList<Integer> order = new ArrayList<Integer>();
     ArrayList<ImageView> hearts = new ArrayList<ImageView>();
     String fail;
 
     int lives;
-    Random random=new Random();
+    Random random = new Random();
     int rand;
     int level;
     String str, code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quick);
-        a=findViewById(R.id.A);
-        b=findViewById(R.id.B);
-        c=findViewById(R.id.C);
-        d=findViewById(R.id.D);
-        e=findViewById(R.id.E);
-        f=findViewById(R.id.F);
-        g=findViewById(R.id.G);
-        h=findViewById(R.id.H);
-        i=findViewById(R.id.I);
-        j=findViewById(R.id.J);
-        k=findViewById(R.id.K);
-        l=findViewById(R.id.L);
-        m=findViewById(R.id.M);
-        n=findViewById(R.id.N);
-        o=findViewById(R.id.O);
-        p=findViewById(R.id.P);
-        q=findViewById(R.id.Q);
-        r=findViewById(R.id.R);
-        s=findViewById(R.id.S);
-        t=findViewById(R.id.T);
-        u=findViewById(R.id.U);
-        v=findViewById(R.id.V);
-        w=findViewById(R.id.W);
-        x=findViewById(R.id.X);
-        y=findViewById(R.id.Y);
-        z=findViewById(R.id.Z);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_quick);
+            a = findViewById(R.id.A);
+            b = findViewById(R.id.B);
+            c = findViewById(R.id.C);
+            d = findViewById(R.id.D);
+            e = findViewById(R.id.E);
+            f = findViewById(R.id.F);
+            g = findViewById(R.id.G);
+            h = findViewById(R.id.H);
+            i = findViewById(R.id.I);
+            j = findViewById(R.id.J);
+            k = findViewById(R.id.K);
+            l = findViewById(R.id.L);
+            m = findViewById(R.id.M);
+            n = findViewById(R.id.N);
+            o = findViewById(R.id.O);
+            p = findViewById(R.id.P);
+            q = findViewById(R.id.Q);
+            r = findViewById(R.id.R);
+            s = findViewById(R.id.S);
+            t = findViewById(R.id.T);
+            u = findViewById(R.id.U);
+            v = findViewById(R.id.V);
+            w = findViewById(R.id.W);
+            x = findViewById(R.id.X);
+            y = findViewById(R.id.Y);
+            z = findViewById(R.id.Z);
 
-        text=findViewById(R.id.text);
-        failed=findViewById(R.id.failed);
+            text = findViewById(R.id.text);
+            failed = findViewById(R.id.failed);
 
-        h1=findViewById(R.id.h1);
-        h2=findViewById(R.id.h2);
-        h3=findViewById(R.id.h3);
-        h4=findViewById(R.id.h4);
-        h5=findViewById(R.id.h5);
-        h6=findViewById(R.id.h6);
-        h7=findViewById(R.id.h7);
+            h1 = findViewById(R.id.h1);
+            h2 = findViewById(R.id.h2);
+            h3 = findViewById(R.id.h3);
+            h4 = findViewById(R.id.h4);
+            h5 = findViewById(R.id.h5);
+            h6 = findViewById(R.id.h6);
+            h7 = findViewById(R.id.h7);
 
-        hearts.add(h1);
-        hearts.add(h2);
-        hearts.add(h3);
-        hearts.add(h4);
-        hearts.add(h5);
-        hearts.add(h6);
-        hearts.add(h7);
+            hearts.add(h1);
+            hearts.add(h2);
+            hearts.add(h3);
+            hearts.add(h4);
+            hearts.add(h5);
+            hearts.add(h6);
+            hearts.add(h7);
 
-        words.add("INTERSTELLAR");
-        words.add("PREDESTINATION");
-        words.add("THE HANGOVER");
-        words.add("GODFATHER");
-        words.add("TITANIC");
-        words.add("ALADDIN");
-        words.add("INCEPTION");
-        words.add("JOHNNY ENGLISH");
-        words.add("ANABELLE");
-        words.add("INCIDIOUS");
-        words.add("TERMINATOR");
-        words.add("LIFE OF PI");
-        words.add("SHUTTER ISLAND");
-        words.add("THE DARK KNIGHT");
-        words.add("FINDING NEMO");
-        words.add("JUMANJI");
-        words.add("THE PRESTIGE");
-        words.add("HOME ALONE");
-        words.add("SHERLOCK HOLMES");
-        words.add("JURASSIC PARK");
+            words.add("INTERSTELLAR");
+            words.add("PREDESTINATION");
+            words.add("THE HANGOVER");
+            words.add("GODFATHER");
+            words.add("TITANIC");
+            words.add("ALADDIN");
+            words.add("INCEPTION");
+            words.add("JOHNNY ENGLISH");
+            words.add("ANABELLE");
+            words.add("INCIDIOUS");
+            words.add("TERMINATOR");
+            words.add("LIFE OF PI");
+            words.add("SHUTTER ISLAND");
+            words.add("THE DARK KNIGHT");
+            words.add("FINDING NEMO");
+            words.add("JUMANJI");
+            words.add("THE PRESTIGE");
+            words.add("HOME ALONE");
+            words.add("SHERLOCK HOLMES");
+            words.add("JURASSIC PARK");
 
-        for(int i=0; i<20; i++)
-            order.add(i);
+            for (int i = 0; i < 20; i++)
+                order.add(i);
 
-        a.setOnClickListener(v -> keyboard("A"));
-        b.setOnClickListener(v -> keyboard("B"));
-        c.setOnClickListener(v -> keyboard("C"));
-        d.setOnClickListener(v -> keyboard("D"));
-        e.setOnClickListener(v -> keyboard("E"));
-        f.setOnClickListener(v -> keyboard("F"));
-        g.setOnClickListener(v -> keyboard("G"));
-        h.setOnClickListener(v -> keyboard("H"));
-        i.setOnClickListener(v -> keyboard("I"));
-        j.setOnClickListener(v -> keyboard("J"));
-        k.setOnClickListener(v -> keyboard("K"));
-        l.setOnClickListener(v -> keyboard("L"));
-        m.setOnClickListener(v -> keyboard("M"));
-        n.setOnClickListener(v -> keyboard("N"));
-        o.setOnClickListener(v -> keyboard("O"));
-        p.setOnClickListener(v -> keyboard("P"));
-        q.setOnClickListener(v -> keyboard("Q"));
-        r.setOnClickListener(v -> keyboard("R"));
-        s.setOnClickListener(v -> keyboard("S"));
-        t.setOnClickListener(v -> keyboard("T"));
-        u.setOnClickListener(v -> keyboard("U"));
-        v.setOnClickListener(v -> keyboard("V"));
-        w.setOnClickListener(v -> keyboard("W"));
-        x.setOnClickListener(v -> keyboard("X"));
-        y.setOnClickListener(v -> keyboard("Y"));
-        z.setOnClickListener(v -> keyboard("Z"));
+            a.setOnClickListener(v -> keyboard("A"));
+            b.setOnClickListener(v -> keyboard("B"));
+            c.setOnClickListener(v -> keyboard("C"));
+            d.setOnClickListener(v -> keyboard("D"));
+            e.setOnClickListener(v -> keyboard("E"));
+            f.setOnClickListener(v -> keyboard("F"));
+            g.setOnClickListener(v -> keyboard("G"));
+            h.setOnClickListener(v -> keyboard("H"));
+            i.setOnClickListener(v -> keyboard("I"));
+            j.setOnClickListener(v -> keyboard("J"));
+            k.setOnClickListener(v -> keyboard("K"));
+            l.setOnClickListener(v -> keyboard("L"));
+            m.setOnClickListener(v -> keyboard("M"));
+            n.setOnClickListener(v -> keyboard("N"));
+            o.setOnClickListener(v -> keyboard("O"));
+            p.setOnClickListener(v -> keyboard("P"));
+            q.setOnClickListener(v -> keyboard("Q"));
+            r.setOnClickListener(v -> keyboard("R"));
+            s.setOnClickListener(v -> keyboard("S"));
+            t.setOnClickListener(v -> keyboard("T"));
+            u.setOnClickListener(v -> keyboard("U"));
+            v.setOnClickListener(v -> keyboard("V"));
+            w.setOnClickListener(v -> keyboard("W"));
+            x.setOnClickListener(v -> keyboard("X"));
+            y.setOnClickListener(v -> keyboard("Y"));
+            z.setOnClickListener(v -> keyboard("Z"));
 
-        startgame();
+            startgame();
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString()+" main", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void startgame() {
+        try {
+            if (order.size() == 0) {
+                Toast.makeText(this, "Congratulations! You are a movie geek!", Toast.LENGTH_SHORT).show();
 
-        if(order.size()==0)
-        {
-            Toast.makeText(this, "Congratulations! You are a movie geek!", Toast.LENGTH_SHORT).show();
+                MainActivity.setTimeout(() -> {
+                    Intent i = new Intent(getApplicationContext(), players.class);
+                    startActivity(i);
+                    finish();
+                }, 3000);
+            }
 
-            MainActivity.setTimeout(() -> {
-                Intent i = new Intent(getApplicationContext(),players.class);
-                startActivity(i);
-                finish();
-            }, 3000);
+            lives = 7;
+            fail = "";
+            failed.setText("Failed Attempts");
+
+            for (int i = 0; i < 7; i++)
+                hearts.get(i).setBackgroundResource(R.drawable.heart);
+
+            rand = random.nextInt(order.size());
+            level = order.get(rand);
+            order.remove(order.indexOf(rand));
+
+            str = words.get(level);
+            code = "";
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != ' ')
+                    code += "*";
+
+                else
+                    code += " ";
+            }
+            text.setText(code);
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString()+" startgame", Toast.LENGTH_LONG).show();
         }
-
-        lives=7;
-        fail="";
-        failed.setText("Failed Attempts");
-
-        for(int i=0; i<7; i++)
-            hearts.get(i).setBackgroundResource(R.drawable.heart);
-
-        rand=random.nextInt(order.size());
-        level=order.get(rand);
-        order.remove(order.indexOf(rand));
-
-        str = words.get(level);
-        code="";
-
-        for(int i=0; i<str.length(); i++) {
-            if (str.charAt(i) != ' ')
-                code += "*";
-
-            else
-                code+=" ";
-        }
-        text.setText(code);
     }
 
     public void keyboard(String a) {
-        boolean flag=false;
+        try {
+            boolean flag = false;
 
-        for(int i=0; i<str.length(); i++) {
-            if (str.charAt(i) == a.charAt(0)) {
-                code = code.substring(0, i) + a.charAt(0) + code.substring(i + 1);
-                flag=true;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == a.charAt(0)) {
+                    code = code.substring(0, i) + a.charAt(0) + code.substring(i + 1);
+                    flag = true;
+                }
             }
-        }
 
-        if(!flag) {
-            lives--;
-            fail=fail+a+" ";
-            failed.setText(fail);
-            hearts.get(lives).setBackgroundResource(R.drawable.lostheart);
-        }
+            if (!flag) {
+                lives--;
+                fail = fail + a + " ";
+                failed.setText(fail);
+                hearts.get(lives).setBackgroundResource(R.drawable.lostheart);
+            }
 
-        text.setText(code);
-        checkfinish();
+            text.setText(code);
+            checkfinish();
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString()+" keyboard", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void checkfinish() {
-        if(code.equals(str))
-        {
-            startgame();
-        }
+        try {
+            if (code.equals(str)) {
+                startgame();
+            }
 
-        if(lives==0)
-        {
-            Toast.makeText(this, "You are a loser!", Toast.LENGTH_SHORT).show();
+            if (lives == 0) {
+                Toast.makeText(this, "You are a loser!", Toast.LENGTH_SHORT).show();
 
-            MainActivity.setTimeout(() -> {
-                Intent i = new Intent(getApplicationContext(),players.class);
-                startActivity(i);
-                finish();
-            }, 1000);
+                MainActivity.setTimeout(() -> {
+                    Intent i = new Intent(getApplicationContext(), players.class);
+                    startActivity(i);
+                    finish();
+                }, 1000);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString()+" checkfinish", Toast.LENGTH_LONG).show();
         }
     }
 }
